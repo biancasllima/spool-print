9eaa63425074bfc0c262708281c3f5f1aa293bc0
Bianca Lima <biancasllima@gmail.com>
2018-11-08 18:24:22 -0300

    adding install instructions

--- install.sh
+++ install.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
-if [ $EUID != 0 ]; then
-    sudo bash "$0" "$@"
-    exit $?
-fi
-
 if ! type "git" > /dev/null; then
   # TODO não exibir mensagem ruim do comando acima
   echo 'You need to install git'
   exit $?
 fi
 
-LIB_FOLDER=/usr/local/cli_example
+[ ! -d /tmp/spool-print ] || rm -rf /tmp/spool-print
+git clone git@github.com:biancasllima/spool-print.git /tmp/spool-print --quiet
 
-if [[ -d $LIB_FOLDER ]]; then
-    echo 'removing past version'
-    rm -rf $LIB_FOLDER
+function install_spool-print {
+    LIB_FOLDER=/usr/local/spool-print
+
+    if [[ -d $LIB_FOLDER ]]; then
+        echo 'removing past version'
+        rm -rf $LIB_FOLDER
+    fi
+
+    mv /tmp/spool-print /usr/local
+    chmod 755 /usr/local/spool-print
+
+    ln -s -f /usr/local/spool-print/main.sh /usr/bin/spool-print
+    chmod 755 /usr/bin/spool-print
+}
+
+if [ $EUID != 0 ]; then
+    FUNC=$(declare -f install_spool-print)
+    sudo bash -c "$FUNC; install_spool-print"
+    echo "Sucess! TODO colocar mensagem"
+    exit $?
 fi
-
-git clone git@github.com:julioleitao/cli_example.git $LIB_FOLDER
--- main.sh
+++ main.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo "RUNNING MAIN"
 case "$1" in
 "print")
     # https://stackoverflow.com/questions/4824590/propagate-all-arguments-in-a-bash-shell-script
+    echo $PWD
+    echo "FIND THE PRINT FILE!!!!"
     bash lib/print.sh $@
     ;;
 "report")
