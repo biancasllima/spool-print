870a188e2321c6ace39525e9db589df9c37c1ca0
Bianca Lima <biancasllima@gmail.com>
2018-11-11 22:26:23 -0300

    Configuring the report shortcut

--- lib/report.sh
+++ lib/report.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-echo 'calling the report command'
+REPORT_FILE_PATH=/home/$(whoami)/.config_lp_report.csv
+
+USUARIOS=$(sort -u -t, -k1,1 $REPORT_FILE_PATH | awk -F "\"*,\"*" '{print $1}')
+
+TIME_FRAME=$(sort -u -t, -k1,2 $REPORT_FILE_PATH | awk -F "\"*,\"*" '{printf "%s %s-",$2, $3}' | sort -u -t, -k1,1)
+
+IFS='-' read -r -a array <<< "$TIME_FRAME"
+
+for FRAME in "${array[@]}"
+do
+    echo "Uso dos usuarios em $FRAME:"
+
+
+    for USER in ${USUARIOS// / } ; do 
+        IFS=' ' read -ra OUT <<< "$FRAME"
+        MONTH=${OUT[0]}
+        YEAR=${OUT[1]}
+        
+        USAGE=$(grep $USER $REPORT_FILE_PATH | grep $MONTH | grep $YEAR | awk -F',' '{x+=$4}END{print x}')
+                
+        re='^[0-9]+$'
+        if ! [[ $USAGE =~ $re ]]; then
+            USAGE=0
+        fi
+        echo "* $USER: $USAGE"
+    done
+done
