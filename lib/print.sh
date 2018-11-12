d7d5517532c9afcbee0939b1480dfba2bd54d761
Bianca Lima <biancasllima@gmail.com>
2018-11-11 21:15:36 -0300

    Checking user quote

--- lib/print.sh
+++ lib/print.sh
@@ -6,8 +6,10 @@ FILE_PATH=$3
 
 MY_USER=$(whoami)
 REPORT_FILE_PATH=/home/$MY_USER/.config_lp_report.csv
+USERS_FILE_PATH=/home/$MY_USER/.config_lp_users.csv
 
-TIMESTAMP=$(date "+%B/%Y")
+MONTH=$(date "+%B")
+YEAR=$(date "+%Y")
 
 FILE_LINES=`wc -l $FILE_PATH | awk '{ print $1 }'`
 PAGES_LINES=$(((FILE_LINES + 59) / 60))
@@ -21,4 +23,13 @@ else
   NUM_PAGES=$PAGES_LINES
 fi
 
-echo "$LP_USER,$TIMESTAMP,$NUM_PAGES" >> $REPORT_FILE_PATH
+
+
+# saber qual eh o valor da conta do usuario
+USER_QUOTE=$(grep julio.jlmj $USERS_FILE_PATH | awk -F"," '{print $2}')
+echo $USER_QUOTE
+# if []; then
+
+# else
+#     echo "$LP_USER,$MONTH,$YEAR,$NUM_PAGES" >> $REPORT_FILE_PATH
+# fi
