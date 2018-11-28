4ad1f4955ca7ca65e5c26aa362c9d3c6e5d7d07a
Bianca Lima <biancasllima@gmail.com>
2018-11-28 17:31:44 -0300

    Reminders and test.sh

--- lib/test.sh
+++ lib/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This bash script will be used to test commands and bash utilities
+# Do such thing to maintain the projects behavior
+
 echo $INIT
 
 # salvar da mesma forma que os demais arquivos os logs
